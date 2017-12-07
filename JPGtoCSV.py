30b256401fe38db4bf01f4cb0f8ccab129774d8b
Caelean Barnes <caeleanb@gmail.com>
2017-12-07 09:59:36 +0100

    write to Data/

--- JPGtoCSV.py
+++ JPGtoCSV.py
@@ -8,7 +8,8 @@ import sys
 	'Python JPGtoCSV.py -v' : process image to be verified in /test
 '''
 indir = 'images/'
-fname = 'csv/data.csv'
+fname = 'Data/data.csv'
+fmode = 'a'
 # process junk data
 label = '0'
 # process training data
@@ -17,22 +18,22 @@ if sys.argv[1] == '-t':
 # verify image
 elif sys.argv[1] == '-v':
 	indir = 'test/'
-	fname = 'csv/verify.csv'
-file = open(fname, 'a')
-for _, __, filenames in os.walk(indir):
-	for f in filenames:
-		if f[-3:] == 'jpg':
-			ans = ''
-			img = Image.open(indir + f)
-			img_array = numpy.array(img)
-			for row in img_array:
-				for column in row:
-					for value in column:
-						if value < 10:
-							ans += '00'
-						elif value < 100:
-							ans += '0'
-						ans += str(value) + ','
-			ans += label + '\n'
-			file.write(ans)
-file.close()
+	fname = 'Data/verify.csv'
+	fmode = 'w'
+with open(fname, fmode) as file:
+	for _, __, filenames in os.walk(indir):
+		for f in filenames:
+			if f[-3:] == 'jpg':
+				ans = ''
+				img = Image.open(indir + f)
+				img_array = numpy.array(img)
+				for row in img_array:
+					for column in row:
+						for value in column:
+							if value < 10:
+								ans += '00'
+							elif value < 100:
+								ans += '0'
+							ans += str(value) + ','
+				ans += label + '\n'
+				file.write(ans)
