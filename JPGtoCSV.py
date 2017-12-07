5113d046a637c1223276daea16d19f000e10aa9f
Caelean Barnes <caeleanb@gmail.com>
2017-12-07 09:10:54 +0100

    JPG to CSV processing for /images

---
+++ JPGtoCSV.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import numpy
+import os
+
+file = open('data.csv', 'w')
+indir = './images/'
+for _, __, filenames in os.walk(indir):
+	for f in filenames:
+		if f[-3:] == 'jpg':
+			ans = ''
+			img = Image.open(indir + f)
+			img_array = numpy.array(img)
+			for row in img_array:
+				for column in row:
+					for value in column:
+						if value < 10:
+							ans += '00'
+						elif value < 100:
+							ans += '0'
+						ans += str(value) + ','
+			ans += '1\n'
+			file.write(ans)
+file.close()
