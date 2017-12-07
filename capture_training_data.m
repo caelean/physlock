eecb18b1548040f6cd4dd3246f43c111eb63c4cb
Caelean Barnes <caeleanb@gmail.com>
2017-12-07 09:55:00 +0100

    variable number of images to capture

--- capture_training_data.m
+++ capture_training_data.m
@@ -1,7 +1,7 @@
-function capture_training_data()
+function capture_training_data(num_images)
 
 vid = videoinput ('macvideo',2);
-vid.framesPerTrigger = 100;
+vid.framesPerTrigger = num_images;
 set(vid,'LoggingMode','disk');
 avi = VideoWriter('./physlock/video/training.avi');
 set(vid,'DiskLogger',avi);
@@ -19,7 +19,7 @@ n=numFrames;
 for i = 1:1:n
     frame = read(vid,i);
     frame = imresize(frame, 0.125);
-    imwrite(frame,['./physlock/images/Image' int2str(i), '.jpg']);
+    imwrite(frame,['./physlock/images/image' int2str(i), '.jpg']);
     im(i)=image(frame);
 end
 delete(vid);
