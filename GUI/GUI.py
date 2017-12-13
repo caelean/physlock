e6b9b6eedac309716db8f0bee7f98a5947cf1ad0
Ninosauleo <ninosauleo@gmail.com>
2017-12-13 14:56:40 +0100

    with pop_up dialog

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,7 +1,7 @@
 import os
 import errno
 from cv2 import *
-from os.path import expanduser, join
+import tkMessageBox
 
 # RELATIVE PATH TO THE DIRECTORY
 fileDir = os.path.dirname(os.path.realpath('__file__'))
@@ -12,12 +12,14 @@ filename = os.path.join(fileDir + '/New_secret_pic')
 def start_cam():
     # INDEX 0 FOR WEBCAM AND 1 FOR USB WEBCAM.
     cam = cv2.VideoCapture(0)
-    # WINDOW_AUTOSIZE (the window size is automatically adjusted to fit the displayed image)
-    cv2.namedWindow("test", WINDOW_AUTOSIZE)
     # FUNCTION TO TAKE A SCREEN_SHOT
     take_screen_shot(cam, cv2)
     # RELEASE IMAGE
     destroy_cam(cam, cv2)
+
+
+def pop_window(title, message):
+    tkMessageBox.showinfo(title, message)
 
 
 def take_screen_shot(cam, cv2):
@@ -27,21 +29,26 @@ def take_screen_shot(cam, cv2):
         # RESIZE THE IMAGE FRAME
         resized_image = cv2.resize(frame, (160, 90)) #resize the image frame
         #DISPLAY THE IMAGE
-        cv2.imshow("TAKE A SCREEN SHOT", frame)
+        window_name = "TAKE A SCREEN SHOT"
+        cv2.imshow(window_name, frame)
+        # MOVE THE WINDOW TO THE CENTER
+        cv2.moveWindow(window_name, 100, 0)
         # IF NOTHING BREAK THE LOOP
         if not ret:
             break
-        # INPUT KEY
+        # WAIT FOR INPUT KEY
         k = cv2.waitKey(1)
         # ESC PRESSED
         if k % 256 == 27:
             # IF ESC BREAK THE LOOP
             print "bye! "
+            destroy_cam(cam, cv2)
+            exit()
             break
         # SPACE PRESSED
         elif k % 256 == 32:
             # IMAGE NAME
-            img_name = "picture.png"
+            img_name = "picture.jpg"
             # WRITE IMAGE TO RELATIVE PATH
             cv2.imwrite(os.path.join(filename, img_name), resized_image)
 
@@ -52,6 +59,7 @@ def destroy_cam(cam, cv2):
 
 
 def main():
+    pop_window("INSTRUCTIONS", "PRESS SPACE TO VERIFY AND ESC TO EXIT THE PROGRAM")
     start_cam()
 
 
